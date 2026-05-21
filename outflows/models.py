ac0f79af02cb29a5c7cd444416cab9fd76a2287e
joao <dev@monitoo.com.br>
2026-05-21 10:23:01 -0300

    feat: implementando o app e model outflows

--- outflows/models.py
+++ outflows/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from products.models import Product
-from suppliers.models import Supplier
 
 
-class Inflow(models.Model):
-    supplier = models.ForeignKey(Supplier, on_delete=models.PROTECT, related_name='inflows')
-    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='inflows')
+class Outflow(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='outlows')
     quantity = models.IntegerField()
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -13,6 +11,6 @@ class Inflow(models.Model):
 
     class Meta:
         ordering = ['-created_at']
-    
+
     def __str__(self):
         return str(self.product)
